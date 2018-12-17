a78ed8d099992a320771f9c7aacfd1b575c869c2
sgiron <shantgiron@gmail.com>
2018-12-17 11:11:34 -0400

    base proyecto

---
+++ src/main/java/ce/pucmm/finalWeb/FinalWebApplication.java
@@ -0,0 +1,14 @@
+package ce.pucmm.finalWeb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FinalWebApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FinalWebApplication.class, args);
+	}
+
+}
+
